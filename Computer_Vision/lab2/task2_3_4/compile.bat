664183d0f8bdfd9149c0fde1a80ea203bb63235a
matteodegobbi <matteodegobbi2001@gmail.com>
2024-03-20 23:55:46 +0100

    robe del report quasi finite

--- Computer_Vision/lab2/task2_3_4/compile.bat
+++ Computer_Vision/lab2/task2_3_4/compile.bat
@@ -1,1 +1,1 @@
-g++ -H -I"C:\opencv\install\include" main.cpp ImageFilters.cpp -o main -Wall -L"C:\opencv\install\x64\mingw\lib" -lopencv_highgui490 -lopencv_core490 -lopencv_imgcodecs490 -lopencv_imgproc490 > out.txt
+g++ -I"C:\opencv\install\include" -O3 main.cpp ImageFilters.cpp -o main -Wall -L"C:\opencv\install\x64\mingw\lib" -lopencv_highgui490 -lopencv_core490 -lopencv_imgcodecs490 -lopencv_imgproc490
--- Computer_Vision/lab2/task5_6/main.cpp
+++ Computer_Vision/lab2/task5_6/main.cpp
@@ -1,3 +1,4 @@
+#include "opencv2/core/types.hpp"
 #include <iostream>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -8,11 +9,16 @@ void draw_hist(const cv::Mat &hist, cv::Mat &histImage, int histSize, int width,
   cv::normalize(hist, hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
   for (int i = 1; i < histSize; i++) {
     using namespace cv;
-    line(histImage,
-         Point(bin_width * (i - 1), height - cvRound(hist.at<float>(i - 1))),
-         Point(bin_width * (i), height - cvRound(hist.at<float>(i))), color, 3);
+    rectangle(
+        histImage,
+        Point(bin_width * (i - 1), height - cvRound(hist.at<float>(i - 1))),
+        Point(bin_width * (i), height), color, 1);
   }
 }
+// line(histImage,
+//      Point(bin_width * (i - 1), height - cvRound(hist.at<float>(i - 1))),
+//      Point(bin_width * (i), height - cvRound(hist.at<float>(i))), color,
+//      3);
 int main(int argc, char **argv) {
   cv::Mat img = cv::imread("Garden.jpg");
 
@@ -20,31 +26,34 @@ int main(int argc, char **argv) {
     std::cout << "Image not found" << std::endl;
     return 2;
   }
-  cv::Mat out;
+  cv::Mat originalGs;
   cv::Mat hist;
-  cv::cvtColor(img, out, cv::COLOR_RGB2GRAY);
-  cv::imwrite("Garden_grayscale.png", out);
+  cv::cvtColor(img, originalGs, cv::COLOR_RGB2GRAY);
+  cv::imwrite("Garden_grayscale.png", originalGs);
 
-  float range[] = {0, 256};
+  float range[2] = {0, 256};
   const float *histRange[] = {range};
   int histSize = 256;
   bool uniform = true, accumulate = false;
-  calcHist(&out, 1, 0, cv::Mat(), hist, 1, &histSize, histRange, uniform,
+  calcHist(&originalGs, 1, 0, cv::Mat(), hist, 1, &histSize, histRange, uniform,
            accumulate);
-  int hist_w = 1500, hist_h = 1500;
+  int hist_w = 1200, hist_h = 800;
   cv::Mat histImage(hist_h, hist_w, CV_8UC3, cv::Scalar(255, 255, 255));
 
   draw_hist(hist, histImage, histSize, hist_w, hist_h, cv::Scalar(100, 0, 0));
   cv::imshow("hist before equalization", histImage);
 
   cv::Mat equalizedGs;
-  cv::equalizeHist(out, equalizedGs);
+  cv::equalizeHist(originalGs, equalizedGs);
+  histImage = cv::Mat(hist_h, hist_w, CV_8UC3, cv::Scalar(255, 255, 255));
   calcHist(&equalizedGs, 1, 0, cv::Mat(), hist, 1, &histSize, histRange,
            uniform, accumulate);
   draw_hist(hist, histImage, histSize, hist_w, hist_h, cv::Scalar(0, 0, 200));
   cv::imshow("hist after equalization", histImage);
-  cv::imshow("original image", out);
+  cv::imshow("original image", originalGs);
+
   cv::imshow("equalized image", equalizedGs);
+  cv::imwrite("equalized.png", equalizedGs);
   cv::waitKey(0);
   return 0;
 }
