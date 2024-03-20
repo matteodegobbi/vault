cf91b7c15ac8b893fa6055b7f0eea089c54491a6
matteodegobbi <matteodegobbi2001@gmail.com>
2024-03-20 18:22:13 +0100

    lezioni mercoledi 20

--- Computer_Vision/lab2/task2_3_4/main.cpp
+++ Computer_Vision/lab2/task2_3_4/main.cpp
@@ -7,8 +7,9 @@ int main(int argc, char **argv) {
   std::string imgname;
   if (argc < 2)
     imgname = "Garden.jpg";
-  else imgname = argv[1];
-  std::cout<<imgname << std::endl;
+  else
+    imgname = argv[1];
+  std::cout << imgname << std::endl;
   cv::Mat img = cv::imread(imgname);
 
   if (img.empty()) {
@@ -19,12 +20,13 @@ int main(int argc, char **argv) {
   cv::cvtColor(img, gray, cv::COLOR_RGB2GRAY);
 
   cv::Mat out = gray.clone();
-  cv::Mat outblur; // = gray.clone();
-  medianFilter(gray, out, 7);
+  cv::Mat outblur = gray.clone();
+  // cv::resize(gray, out, cv::Size(), 0.75, 0.75);
+  medianFilter(gray, out, 3);
   cv::GaussianBlur(gray, outblur, cv::Size(3, 9), 10);
-  cv::imshow("window1", out);
+  // cv::imshow("window1", out);
   cv::imshow("gaussianblur", outblur);
-  cv::imwrite("task2/outmin.png", out);
+  // cv::imwrite("task2/outmin.png", out);
   cv::waitKey(0);
   return 0;
 }
