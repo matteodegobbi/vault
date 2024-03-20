a5be08e7260abae0d25f61cd94d48535bcb7f466
matteodegobbi <matteodegobbi2001@gmail.com>
2024-03-20 18:22:13 +0100

    lezioni mercoledi 20

--- Computer_Vision/lab2/task2_3_4/compile.bat
+++ Computer_Vision/lab2/task2_3_4/compile.bat
@@ -1,1 +1,1 @@
-g++ main.cpp ImageFilters.cpp -O3 -o main -IC:\opencv\build\include -LC:\opencv\build\x64\mingw\bin -llibopencv_highgui455 -llibopencv_imgproc455 -llibopencv_core455 -llibopencv_imgcodecs455
+g++ -I"C:\opencv\install\include" main.cpp ImageFilters.cpp -o main -Wall -L"C:\opencv\install\x64\mingw\lib" -lopencv_highgui490 -lopencv_core490 -lopencv_imgcodecs490 -lopencv_imgproc490
--- Computer_Vision/lab2/task2_3_4/compile.sh
+++ Computer_Vision/lab2/task2_3_4/compile.sh
@@ -1,3 +1,1 @@
-
-
 g++ main.cpp ImageFilters.cpp -O3 -o main -I/usr/local/include/opencv4 -lopencv_highgui -lopencv_core -lopencv_imgcodecs -lopencv_imgproc
