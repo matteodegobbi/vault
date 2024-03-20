88b7c6ad22d480628aa93e269b7cc93cc2593109
matteodegobbi <matteodegobbi2001@gmail.com>
2024-03-20 22:27:35 +0100

    messo a posto clagnd

--- Computer_Vision/lab2/task2_3_4/compile.bat
+++ Computer_Vision/lab2/task2_3_4/compile.bat
@@ -1,1 +1,1 @@
-g++ -I"C:\opencv\install\include" main.cpp ImageFilters.cpp -o main -Wall -L"C:\opencv\install\x64\mingw\lib" -lopencv_highgui490 -lopencv_core490 -lopencv_imgcodecs490 -lopencv_imgproc490
+g++ -H -I"C:\opencv\install\include" main.cpp ImageFilters.cpp -o main -Wall -L"C:\opencv\install\x64\mingw\lib" -lopencv_highgui490 -lopencv_core490 -lopencv_imgcodecs490 -lopencv_imgproc490 > out.txt
