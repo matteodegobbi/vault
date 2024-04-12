1c72acef8affbb3d38c9df218e704d6b8e332581
matteodegobbi <matteodegobbi2001@gmail.com>
2024-04-12 23:32:48 +0200

    finito report lab 4

--- Computer_Vision/lab4/task1/task1.cpp
+++ Computer_Vision/lab4/task1/task1.cpp
@@ -13,6 +13,7 @@ struct userdata_t {
   int *threshold2;
 };
 constexpr int canny_max_threshold = 500;
+
 static void on_trackbar(int _, void *userdata) {
   userdata_t data = *static_cast<userdata_t *>(userdata);
   double thres1 = (double)*data.threshold1;
@@ -27,21 +28,17 @@ int main(void) {
     cout << "Error loading src1 \n";
     return -1;
   }
-  namedWindow("Canny output", WINDOW_AUTOSIZE); // Create Window
-  char TrackbarName[50];
-  snprintf(TrackbarName, sizeof(TrackbarName), "Alpha x %d",
-           canny_max_threshold);
+  namedWindow("Canny output", WINDOW_AUTOSIZE);
   int threshold1 = 10;
   int threshold2 = 20;
   userdata_t data = {.in = src1,
                      .out = dst,
                      .threshold1 = &threshold1,
                      .threshold2 = &threshold2};
-  createTrackbar("track1", "Canny output", &threshold1, canny_max_threshold,
-                 on_trackbar, &data);
-  createTrackbar("track2", "Canny output", &threshold2, canny_max_threshold,
-                 on_trackbar, &data);
-  // on_trackbar(threshold1, &data);
+  createTrackbar("threshold 1", "Canny output", &threshold1,
+                 canny_max_threshold, on_trackbar, &data);
+  createTrackbar("threshold 2", "Canny output", &threshold2,
+                 canny_max_threshold, on_trackbar, &data);
   waitKey(0);
   return 0;
 }
